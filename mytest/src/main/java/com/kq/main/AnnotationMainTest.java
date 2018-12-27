a6bd3568f2c7a5f5c4d805b7c81c27edd9b71052
king <king@qq.com>
2018-12-27 15:41:50 +0800

    add log4j

--- mytest/src/main/java/com/kq/main/AnnotationMainTest.java
+++ mytest/src/main/java/com/kq/main/AnnotationMainTest.java
@@ -2,6 +2,8 @@ package com.kq.main;
 
 import com.kq.bean.Employee;
 import com.kq.config.MyConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -12,7 +14,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class AnnotationMainTest {
 
+	private static Logger logger = LoggerFactory.getLogger(AnnotationMainTest.class);
+
 	public static void main(String args[]) {
+
+		logger.info("start AnnotationMainTest");
 
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MyConfiguration.class);
 		Employee e = ctx.getBean(Employee.class);
